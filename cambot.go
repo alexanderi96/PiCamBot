696b4ae20b6870c0626a35f2fca2943e3c49313e
alexanderi96 <alessandro.ianne96@gmail.com>
2023-01-12 17:36:56 +0100

    Added user filtering

--- cambot.go
+++ cambot.go
@@ -1,53 +1,85 @@
 package main
 
 import (
-	"github.com/NicoNex/echotron"
+	"github.com/NicoNex/echotron/v3"
 	"os/exec"
 	"time"
 	"os"
 	"log"
 	"strconv"
+	"strings"
+
+	_"embed"
 )
 
 type bot struct {
-	chatId int64
-	echotron.Api
+	chatID int64
+	echotron.API
 }
 
-func newBot(api echotron.Api, chatId int64) echotron.Bot {
+
+var (
+	//go:embed token
+	token string
+
+	//go:embed admin
+	admin string
+)
+
+func init() {
+	if len(token) == 0 {
+		log.Fatal("Empty token file")
+	}
+
+	if len(admin) == 0 {
+		log.Fatal("Empty admin file")
+	}
+}
+
+func newBot(chatID int64) echotron.Bot {
     return &bot{
-            chatId,
-            api,
+            chatID,
+            echotron.NewAPI(token),
         }
 }
 
 func (b *bot) Update(update *echotron.Update) {
-	path := "./archive/" + strconv.FormatInt(b.chatId, 10) + "/"
+	path := "./archive/" + strconv.FormatInt(b.chatID, 10) + "/"
 	b.checkFolder(path)
 
-	log.Println("Message recieved from: " + strconv.FormatInt(b.chatId, 10))
-	if update.Message.Text == "/start" {
-	        b.SendMessage("Ready to take a shot!", b.chatId)
-	} else if update.Message.Text == "/shot" {
-		b.SendMessage("Taking a shot", b.chatId)
-		date := time.Now().Unix()
-		strdate := strconv.FormatInt(date, 10)
-		name := "pic" + strdate + ".jpg"
-		_, err := exec.Command("raspistill", "-o",  path + name).Output()
-		if err == nil {
-			b.SendDocument(path + name, name, b.chatId)
-		} else {
-			log.Fatal(err)
-			b.SendMessage("error", b.chatId)
+	log.Println("Message recieved from: " + strconv.FormatInt(b.chatID, 10))
+	
+	if strconv.Itoa(int(b.chatID)) != admin {
+		b.SendMessage("📷", b.chatID, nil)
+	} else {
+		msg:=strings.Split(update.Message.Text, " ")
+		command:=msg[0]
+		params:="-o"
+		if len(msg) > 1 {
+			params+=" "+msg[1]
 		}
+		if command == "/start" {
+		        b.SendMessage("Ready to take a shot!", b.chatID, nil)
+		} else if update.Message.Text == "/shot" {
+			b.SendMessage("Taking a shot", b.chatID, nil)
+			date := time.Now().Unix()
+			strdate := strconv.FormatInt(date, 10)
+			name := "pic" + strdate + ".jpg"
+			_, err := exec.Command("libcamera-still", "-n", params,  path + name).Output()
+			if err == nil {
+				b.SendDocument(echotron.NewInputFilePath(path + name), b.chatID, nil)
+			} else {
+				log.Fatal(err)
+				b.SendMessage("error", b.chatID, nil)
+			}
+		}	
 	}
 }
 
 func (b *bot) checkFolder(path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
-		
 		os.Mkdir(path, 0755)
-		log.Println("Directory for user " + strconv.FormatInt(b.chatId, 10) + " created")
+		log.Println("Directory for user " + strconv.FormatInt(b.chatID, 10) + " created")
 	}
 }
 
@@ -55,8 +87,8 @@ func main() {
 	if _, err := os.Stat("archive"); os.IsNotExist(err) {
 		os.Mkdir("archive", 0755)
 	}
-	dsp := echotron.NewDispatcher("your-telegram-token", newBot)
+	dsp := echotron.NewDispatcher(token, newBot)
 	log.Println("Running CamBot")
-	dsp.Run()
+	log.Println(dsp.Poll())
 }
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alexanderi96/PiCamBot
+
+go 1.19
+
+require github.com/NicoNex/echotron/v3 v3.21.0
